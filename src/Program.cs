ce3a544c898cfecd53cfa013ed0d8032aec15020
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-12 14:03:18 -0300

    estrutura inicial da classe Program

--- src/Program.cs
+++ src/Program.cs
@@ -4,3 +4,17 @@ Referencia ao pacote SQLite:
 $ dotnet add package System.Data.SQLite
 https://www.nuget.org/packages/System.Data.SQLite
 */
+
+using System;
+using System.Data.SQLite;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
